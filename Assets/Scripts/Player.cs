4bd12669d221456f6a70691d1d706fc68d50228d
jedi-narwhal <soniasnlep@yahoo.com>
2026-01-07 10:34:05 -0800

    first commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,41 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft=_numSeeds;
     }
 
     private void Update()
     {
+ 
+        if ((Input.GetKey(KeyCode.W) ))
+        {
+            _playerTransform.Translate(Vector2.up);
+        }
+        else if ((Input.GetKey(KeyCode.A) ))
+        {
+            _playerTransform.Translate(Vector2.left);
+        }
+        else if((Input.GetKey(KeyCode.S) ))
+        {
+            _playerTransform.Translate(Vector2.down);
+        }
+        else if ((Input.GetKey(KeyCode.D) ))
+        {
+            _playerTransform.Translate(Vector2.right);
+        }
+
+ 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            PlantSeed();
         
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        Instantiate(_plantPrefab,transform.position, transform.rotation);
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 }
